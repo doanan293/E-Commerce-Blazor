8f43bed6d89e00157df4d2f2a3b6321968f22096
Doan Van An <andv@vcsvietnam.vn>
2025-07-05 08:46:56 +0700

    up

--- eCommerceApp.Domain/Interfaces/Cart/ICartService.cs
+++
@@ -1,7 +0,0 @@
-﻿using eCommerceApp.Domain.Entities.Cart;
-
-namespace eCommerceApp.Domain.Interfaces.Cart {
-    public interface ICartService {
-        Task<int> SaveCheckoutHistory(IEnumerable<CreateAchieve> checkouts);
-    }
-}
